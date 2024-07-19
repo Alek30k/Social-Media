ea43c0415fe736438195c1bb41ae4d7fae13553d
Alek30k <alemank23k@gmail.com>
2024-07-19 19:44:31 -0300

    last commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
-import { ClerkLoading } from "@clerk/nextjs";
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -60,9 +60,28 @@ const Navbar = () => {
       <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
         <ClerkLoading>
           <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
-
-          <MobileMenu />
         </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className="cursor-pointer">
+              <Image src="/people.png" alt="" width={24} height={24} />
+            </div>
+            <div className="cursor-pointer">
+              <Image src="/messages.png" alt="" width={20} height={20} />
+            </div>
+            <div className="cursor-pointer">
+              <Image src="/notifications.png" alt="" width={20} height={20} />
+            </div>
+            {/* <UserButton /> */}
+          </SignedIn>
+          <SignedOut>
+            <div className="flex items-center gap-2 text-sm">
+              <Image src="/login.png" alt="" width={20} height={20} />
+              <Link href="/sign-in">Login/Register</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
+        <MobileMenu />
       </div>
     </div>
   );
