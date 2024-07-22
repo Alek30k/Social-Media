205e609b75e365dab88ab923c678e86d2c146f71
Alek30k <alemank23k@gmail.com>
2024-07-22 14:48:11 -0300

    last commit

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,6 +1,6 @@
 // import prisma from "@/lib/client";
 // import { auth } from "@clerk/nextjs/server";
-// import Image from "next/image";
+import Image from "next/image";
 // import StoryList from "./StoryList";
 
 const Stories = async () => {
@@ -36,6 +36,16 @@ const Stories = async () => {
     <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
       <div className="flex gap-8 w-max">
         {/* <StoryList stories={stories} userId={currentUserId}/> */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/25003745/pexels-photo-25003745/free-photo-of-ciudad-amanecer-puesta-de-sol-agua.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ale</span>
+        </div>
       </div>
     </div>
   );
