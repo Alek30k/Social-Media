7c3681fd1e5119f44847f865334ade1783ae678a
Alek30k <alemank23k@gmail.com>
2024-07-22 14:56:30 -0300

    last commit

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -36,6 +36,117 @@ const Stories = async () => {
     <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
       <div className="flex gap-8 w-max">
         {/* <StoryList stories={stories} userId={currentUserId}/> */}
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/25003745/pexels-photo-25003745/free-photo-of-ciudad-amanecer-puesta-de-sol-agua.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ale</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/25003745/pexels-photo-25003745/free-photo-of-ciudad-amanecer-puesta-de-sol-agua.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ale</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/25003745/pexels-photo-25003745/free-photo-of-ciudad-amanecer-puesta-de-sol-agua.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ale</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/25003745/pexels-photo-25003745/free-photo-of-ciudad-amanecer-puesta-de-sol-agua.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ale</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/25003745/pexels-photo-25003745/free-photo-of-ciudad-amanecer-puesta-de-sol-agua.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ale</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/25003745/pexels-photo-25003745/free-photo-of-ciudad-amanecer-puesta-de-sol-agua.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ale</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/25003745/pexels-photo-25003745/free-photo-of-ciudad-amanecer-puesta-de-sol-agua.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ale</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/25003745/pexels-photo-25003745/free-photo-of-ciudad-amanecer-puesta-de-sol-agua.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ale</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/25003745/pexels-photo-25003745/free-photo-of-ciudad-amanecer-puesta-de-sol-agua.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ale</span>
+        </div>
+        {/* STORY */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/25003745/pexels-photo-25003745/free-photo-of-ciudad-amanecer-puesta-de-sol-agua.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ale</span>
+        </div>
+        {/* STORY */}
         <div className="flex flex-col items-center gap-2 cursor-pointer">
           <Image
             src="https://images.pexels.com/photos/25003745/pexels-photo-25003745/free-photo-of-ciudad-amanecer-puesta-de-sol-agua.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
