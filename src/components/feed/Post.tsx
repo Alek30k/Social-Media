494ae008830e60ab452ed851c6a99dbb1e94110f
Alek30k <alemank23k@gmail.com>
2024-07-28 23:25:43 -0300

    last commit

--- src/components/feed/Post.tsx
+++ src/components/feed/Post.tsx
@@ -1,11 +1,11 @@
-// const Post = () => {
-//   return (
-//     <div className="flex flex-col gap-4">
-//       {/* USER */}
-//       <div className=""></div>
-//       Post
-//     </div>
-//   );
-// };
+const Post = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      {/* USER */}
+      <div className=""></div>
+      Post
+    </div>
+  );
+};
 
-// export default Post;
+export default Post;
