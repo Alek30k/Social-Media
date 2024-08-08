45e023a2feffedb630abd6595f3a9c9de764188f
Alek30k <alemank23k@gmail.com>
2024-08-07 22:27:33 -0300

    last commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import AddPost from "@/components/AddPost";
-import LeftMenu from "@/components/LeftMenu";
 import RightMenu from "@/components/rigthMenu/RightMenu";
 import Stories from "@/components/Stories";
 import Feed from "@/components/feed/Feed";
+import LeftMenu from "@/components/leftMenu/LeftMenu";
 
 const Homepage = () => {
   return (
     <div className="flex gap-6 pt-6">
       <div className="hidden lg:block w-[20%]">
-        <LeftMenu />
+        <LeftMenu type="home" />
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,12 +1,12 @@
 import Feed from "@/components/feed/Feed";
-import LeftMenu from "@/components/LeftMenu";
+import LeftMenu from "@/components/leftMenu/LeftMenu";
 import RightMenu from "@/components/rigthMenu/RightMenu";
 
 const ProfilePage = () => {
   return (
     <div className="flex gap-6 pt-6">
       <div className="hidden lg:block w-[20%]">
-        <LeftMenu />
+        <LeftMenu type="profile" />
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
